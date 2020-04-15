d0c40769c46963a356e0194fa80801731ff3920c
daffatahtaa <43267539+daffatahtaa@users.noreply.github.com>
2020-04-15 14:35:05 +0700

    praktikum firebase

---
+++ FireBase/firebase/src/firebase.config.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyDU5MeojJSqi_7FLP4h0yA9fcNrKqZ7lb0",
+  authDomain: "fir-53b35.firebaseapp.com",
+  databaseURL: "https://fir-53b35.firebaseio.com",
+  projectId: "fir-53b35",
+  storageBucket: "fir-53b35.appspot.com",
+  messagingSenderId: "860451278327",
+  appId: "1:860451278327:web:b3e9f3427a4e9a939e72f2",
+  measurementId: "G-E662QQD4FK",
+};
