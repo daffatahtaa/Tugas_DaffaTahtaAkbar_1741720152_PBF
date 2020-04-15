b1d71c1a4945a42a7d89291a0c062725e0aaa415
daffatahtaa <43267539+daffatahtaa@users.noreply.github.com>
2020-04-15 16:11:50 +0700

    laporan praktikum dan tugas selesai

--- ReactFirebase/react-firebase/src/Join.js
+++ ReactFirebase/react-firebase/src/Join.js
@@ -1,12 +1,16 @@
 import React, { useState, useContext } from "react";
 import { AuthContext } from "./index";
 import * as firebase from "firebase";
+import * as firebaseui from "firebaseui";
+import firebaseConfig from "./firebase.config";
+
 
 const Join = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-
+  const ui = new firebaseui.auth.AuthUI(firebase.auth());
+  firebase.initializeApp(firebaseConfig);
   const Auth = useContext(AuthContext);
   const handleForm = (e) => {
     e.preventDefault();
@@ -21,6 +25,20 @@ const Join = () => {
       });
   };
 
+  const google = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(function (res) {
+        console.log(res);
+        console.log("naisu");
+      })
+      .catch(function (e) {
+        console.log(e);
+        console.log("no naisu");
+      });
+  };
   return (
     <div>
       <h1>Join</h1>
@@ -41,7 +59,7 @@ const Join = () => {
           placeholder="password"
         />
         <hr />
-        <button class="googleBtn" type="button">
+        <button class="googleBtn" type="button" onClick={() => google()}>
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
             alt="logo"
