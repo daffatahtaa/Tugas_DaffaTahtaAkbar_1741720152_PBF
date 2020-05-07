3aef52769da299d5215420f06349f8868904c28c
daffatahtaa <43267539+daffatahtaa@users.noreply.github.com>
2020-05-07 22:02:58 +0700

    uas

---
+++ UAS/uasframework/firebase/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDU5MeojJSqi_7FLP4h0yA9fcNrKqZ7lb0",
+  authDomain: "fir-53b35.firebaseapp.com",
+  databaseURL: "https://fir-53b35.firebaseio.com",
+  projectId: "fir-53b35",
+  storageBucket: "fir-53b35.appspot.com",
+  messagingSenderId: "860451278327",
+  appId: "1:860451278327:web:22f055a4aff471699e72f2",
+  measurementId: "G-1ZM9Z98SHS",
+};
+
+export default firebaseConfig;
