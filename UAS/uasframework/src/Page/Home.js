fca740354ee7dc54b880c37d404088984964bc39
daffatahtaa <43267539+daffatahtaa@users.noreply.github.com>
2020-05-08 09:38:26 +0700

    home

--- UAS/uasframework/src/Page/Home.js
+++ UAS/uasframework/src/Page/Home.js
@@ -1,12 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./Home.css";
 
 export default function Home() {
   return (
-    <Router>
-      <div>
-          
+    <div>
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap');
+      </style>
+      <div className="quote">
+        <h2>“Success is usually the culmination of controlling failure.”</h2>
       </div>
-    </Router>
+      <div className='name'>
+      <h2>Sylvester Stallone, actor</h2>
+      </div>
+      <a className='lginbtn' href='/Loginuser'>Login</a>
+    </div>
   );
 }
--- UAS/uasframework/src/Page/Nav.js
+++ UAS/uasframework/src/Page/Nav.js
@@ -1,11 +1,21 @@
 import React from "react";
-import "./App.css";
 import { Link } from "react-router-dom";
+import "./Nav.css";
 
 export default function Nav() {
-    return(
-        <nav>
-            
-        </nav>
-    )
+  return (
+    <nav href='Nav.css'>
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css2?family=Jost:wght@560&display=swap');
+      </style>
+      <ul className="base">
+        <li className="logo-placeholder">
+          <Link to="/" className="brand">
+            <a className='navBrand'>WRKOUT</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
