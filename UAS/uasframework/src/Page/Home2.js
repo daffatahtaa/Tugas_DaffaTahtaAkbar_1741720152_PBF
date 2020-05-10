720a64481825a808a87613812828a65db2932aca
daffatahtaa <43267539+daffatahtaa@users.noreply.github.com>
2020-05-11 01:58:19 +0700

    home 2
    Workout,
    Meal prrp

--- UAS/uasframework/src/Page/Home2.js
+++ UAS/uasframework/src/Page/Home2.js
@@ -10,7 +10,7 @@ class Home2 extends React.Component {
   render() {
     return (
       <div>
-        <h4 className="info">Scroll ke samping untuk halaman berikutnya</h4>
+        <h4 className="info">Slide for next page</h4>
         <style>
           @import
           url('https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300&display=swap');
@@ -19,23 +19,18 @@ class Home2 extends React.Component {
           <section>
             <div className="page1">
               <h1>Menu</h1>
-              <p className='title-sm'>Pilih Paket Workout :</p>
+              <p className="title-sm">Choose Workout Plan :</p>
               <div class="quotes">
                 <div class="card">
-                  <div class="box box1">
+                  <div class="box box2">
                     <p>
-                        <div className='inside'>
-                            <li>
-                                Push up x10
-                            </li>
-                            <li>
-                                Sit up x 15
-                            </li>
-                            <li>
-                                Plank 30 s
-                            </li>
-                        </div>
-                      {" "}
+                      <div className="inside">
+                        <h4>5 Rounds :</h4>
+                        <li>Jumping Jacks x60</li>
+                        <li>Lunges x40</li>
+                        <li>Burpees x20</li>
+                        <li>Break 30sec</li>
+                      </div>{" "}
                     </p>
                     <h2>Easy</h2>
                   </div>
@@ -44,17 +39,32 @@ class Home2 extends React.Component {
                 <div class="card">
                   <div class="box box2">
                     <p>
-                      Your smile will give you a positive countenance that will
-                      make people feel comfortable around you.{" "}
+                      <div>
+                        <h4>5 Rounds :</h4>
+                        <li>Run 7 Min (No Rep)</li>
+                        <li>Plank 30s</li>
+                        <li>Crunch x10</li>
+                        <li>Push Up x10</li>
+                        <li>Break 1 Min</li>
+                      </div>
                     </p>
                     <h2>Medium</h2>
                   </div>
                   <div class="bg"></div>
                 </div>
                 <div class="card">
-                  <div class="box box3">
+                  <div class="box box2">
                     <p>
-                      Before anything else, preparation is the key to success.{" "}
+                      <div>
+                        <h4>8 Rounds :</h4>
+                        <li>Run 10 Min (No Rep)</li>
+                        <li>Plank 30s</li>
+                        <li>Sit up x15</li>
+                        <li>Crunch x10</li>
+                        <li>Push Up x10</li>
+                        <li>Tap Shoulder x15</li>
+                        <li>Break 2 Min</li>
+                      </div>
                     </p>
                     <h2>Hard</h2>
                   </div>
@@ -71,6 +81,46 @@ class Home2 extends React.Component {
                     <h1>Meal</h1>
                   </a>
                 </li>
+                <p className="title-sm">Choose Meal Plan :</p>
+
+                <div class="quotes">
+                  <div class="card">
+                    <div class="box box3">
+                      <p>
+                        <div className="inside">
+                          <h5>Breakfast :</h5>
+                          <li>Wheat Bread</li>
+                        </div>{" "}
+                      </p>
+                      <h2>Healthy</h2>
+                    </div>
+                    <div class="bg"></div>
+                  </div>
+                  <div class="card">
+                    <div class="box box3">
+                      <p>
+                        <div className="inside">
+                          <h5>Breakfast :</h5>
+
+                        </div>
+                      </p>
+                      <h2>Low Carbo</h2>
+                    </div>
+                    <div class="bg"></div>
+                  </div>
+                  <div class="card">
+                    <div class="box box3">
+                      <p>
+                        <div className="inside">
+                          <h5>Breakfast :</h5>
+
+                        </div>
+                      </p>
+                      <h2>Diet</h2>
+                    </div>
+                    <div class="bg"></div>
+                  </div>
+                </div>
               </div>
             </div>
           </section>
@@ -84,6 +134,11 @@ class Home2 extends React.Component {
                 </li>
               </div>
             </div>
+            <footer>
+                <a>
+                    logout
+                </a>
+            </footer>
           </section>
         </div>
       </div>
